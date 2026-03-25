c4fb145c5370ebc0b3a9f79c9e8d4ab71fea1d2a
mitchellkevin <mitchell.scholte04@gmail.com>
2026-03-25 15:20:08 +0100

    Added blender model not correct spot yet

--- scripts/script.js
+++ scripts/script.js
@@ -92,34 +92,34 @@
 //     })();
 
 
-// random ster positie
-const stars = document.querySelectorAll('.star');
+// // random ster positie
+// const stars = document.querySelectorAll('.star');
 
-stars.forEach((star) => {
-const size = Math.random() * 2 + 1; // 1–3 px
-const top = Math.random() * 100;
-const left = Math.random() * 100;
-const hue = Math.random() * 360;
+// stars.forEach((star) => {
+// const size = Math.random() * 2 + 1; // 1–3 px
+// const top = Math.random() * 100;
+// const left = Math.random() * 100;
+// const hue = Math.random() * 360;
 
-// elke ster krijgt zijn eigen parallax-sterkte
-const depth = (Math.random() * 30 - 15).toFixed(2); // -15 t/m 15
+// // elke ster krijgt zijn eigen parallax-sterkte
+// const depth = (Math.random() * 30 - 15).toFixed(2); // -15 t/m 15
 
-star.style.setProperty('--star-size', `${size}px`);
-star.style.setProperty('--star-top', `${top}%`);
-star.style.setProperty('--star-left', `${left}%`);
-star.style.setProperty('--star-color', `oklch(0.7 0.2 ${hue})`);
-star.dataset.depth = depth;
-});
+// star.style.setProperty('--star-size', `${size}px`);
+// star.style.setProperty('--star-top', `${top}%`);
+// star.style.setProperty('--star-left', `${left}%`);
+// star.style.setProperty('--star-color', `oklch(0.7 0.2 ${hue})`);
+// star.dataset.depth = depth;
+// });
 
-document.addEventListener('mousemove', (event) => {
-const centerX = window.innerWidth / 2;
-const centerY = window.innerHeight / 2;
+// document.addEventListener('mousemove', (event) => {
+// const centerX = window.innerWidth / 2;
+// const centerY = window.innerHeight / 2;
 
-stars.forEach((star) => {
-    const depth = Number(star.dataset.depth);
-    const x = (event.clientX - centerX) * depth / 300;
-    const y = (event.clientY - centerY) * depth / 300;
+// stars.forEach((star) => {
+//     const depth = Number(star.dataset.depth);
+//     const x = (event.clientX - centerX) * depth / 300;
+//     const y = (event.clientY - centerY) * depth / 300;
 
-    star.style.transform = `translate(${x}px, ${y}px)`;
-});
-});
+//     star.style.transform = `translate(${x}px, ${y}px)`;
+// });
+// });
